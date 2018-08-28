55c679335228967bcfb40f9e61265ae8b167bd5f
Lucas Cavalcanti <lucas.cavalcanti.santos@gmail.com>
2018-08-28 08:57:44 -0300

    transfer TCP

--- ex2/tcp/client/client.go
+++ ex2/tcp/client/client.go
@@ -2,31 +2,83 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net"
 	"os"
+	"strconv"
+	"strings"
 )
+
+// BUFFERSIZE for file transfer
+const BUFFERSIZE = 1024
 
 func main() {
 	if len(os.Args) != 2 {
 		fmt.Fprintf(os.Stderr, "Usage: %s host:port ", os.Args[0])
 		os.Exit(1)
 	}
-	service := os.Args[1]
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+	address := os.Args[1]
+
+	conn, err := net.Dial("tcp", address)
+	// conn, err := net.Dial("udp", address)
 	checkError(err)
-	conn, err := net.DialTCP("tcp", nil, tcpAddr)
-	checkError(err)
-	// _, err = conn.Write([]byte("HEAD / HTTP/1.0\r\n\r\n"))
-	// checkError(err)
-	result, err := ioutil.ReadAll(conn)
-	checkError(err)
-	fmt.Println(string(result))
+	defer conn.Close()
+
+	requestFileTCP("lucas.txt", conn)
+
 	os.Exit(0)
 }
+
+func requestFileTCP(fileName string, conn net.Conn) {
+	// Sending File Name
+	_, err := conn.Write([]byte(fillString(fileName, 50)))
+	checkError(err)
+
+	// Getting File Size
+	bufferFileSize := make([]byte, 15)
+	_, err = conn.Read(bufferFileSize)
+	checkError(err)
+	fileSize, _ := strconv.ParseInt(strings.Trim(string(bufferFileSize), ":"), 10, 64)
+	fmt.Println("FileSize: ", fileSize)
+
+	// Getting File:
+	file, err := os.Create(fileName)
+	checkError(err)
+	defer file.Close()
+
+	var recSize int64
+	recSize = 0
+	for {
+		if (fileSize - recSize) < BUFFERSIZE {
+			io.CopyN(file, conn, (fileSize - recSize))
+			conn.Read(make([]byte, (recSize+BUFFERSIZE)-fileSize))
+			recSize = fileSize
+			break
+		}
+		io.CopyN(file, conn, BUFFERSIZE)
+		recSize += BUFFERSIZE
+		if recSize == fileSize {
+			break
+		}
+	}
+
+}
+
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 	}
+}
+
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
 }
--- ex2/tcp/server/server.go
+++ ex2/tcp/server/server.go
@@ -2,35 +2,83 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 	"os"
-	"time"
+	"strconv"
+	"strings"
 )
 
+// BUFFERSIZE for file transfer
+const BUFFERSIZE = 1024
+
+// Main Server
 func main() {
 	service := ":8080"
-	tcpAddr, err := net.ResolveTCPAddr("tcp4", service)
+
+	listener, err := net.Listen("tcp", service)
+	// listener, err := net.Listen("udp", service)
 	checkError(err)
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	checkError(err)
+
 	for {
 		conn, err := listener.Accept()
+
 		if err != nil {
 			continue
 		}
-		go handleClient(conn)
+		go handleClientTCP(conn)
 	}
 }
 
-func handleClient(conn net.Conn) {
+func handleClientTCP(conn net.Conn) {
 	defer conn.Close()
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime)) // don't care about return value
+	// Getting File Name
+	bufferFileName := make([]byte, 50)
+	_, err := conn.Read(bufferFileName)
+	checkError(err)
+	fileName := strings.Trim(string(bufferFileName), ":")
+	fmt.Println("FileName: ", fileName)
+
+	// Read File and Get Its Size!
+	file, err := os.Open(fileName)
+	checkError(err)
+	defer file.Close()
+	fileInfo, err := file.Stat()
+	checkError(err)
+
+	// Sending Size
+	fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 15)
+	_, err = conn.Write([]byte(fileSize))
+	checkError(err)
+
+	// Sending File:
+	sendBuffer := make([]byte, BUFFERSIZE)
+	for {
+		_, err = file.Read(sendBuffer)
+		if err == io.EOF {
+			break
+		} else {
+			checkError(err)
+		}
+		conn.Write(sendBuffer)
+	}
 	// we're finished with this client
 }
+
 func checkError(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		fmt.Fprintf(os.Stderr, "Fatal error: %s ", err.Error())
 		os.Exit(1)
 	}
+}
+func fillString(retunString string, toLength int) string {
+	for {
+		lengtString := len(retunString)
+		if lengtString < toLength {
+			retunString = retunString + ":"
+			continue
+		}
+		break
+	}
+	return retunString
 }
