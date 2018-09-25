a2b6351c12e8664bf262ed78ef6bd6d711883e9a
Roberto Fernandes <betocostaf@gmail.com>
2018-09-25 18:21:31 -0300

    Changing rcp client to test 1k times

--- ex4/rpc/client/client.go
+++ ex4/rpc/client/client.go
@@ -4,22 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/rpc"
+	"os"
+	"time"
 
 	"./server"
 )
 
 func main() {
-	client, err := rpc.DialHTTP("tcp", "localhost:8081")
-	if err != nil {
-		log.Fatal("dialing:", err)
-	}
+	filename := "rcp-1k.txt"
+	file, err := os.Create(filename)
+	checkError(err)
+	for i := 0; i < 1000; i++ {
+		time1 := time.Now()
 
-	// Synchronous call
-	args := &server.Args{"HELLOWORLD"}
-	var reply string
-	err = client.Call("Str.Lower", args, &reply)
-	if err != nil {
-		log.Fatal("call error:", err)
+		client, err := rpc.DialHTTP("tcp", "localhost:8081")
+		if err != nil {
+			log.Fatal("dialing:", err)
+		}
+
+		// Synchronous call
+		args := &server.Args{"HELLOWORLD"}
+		var reply string
+		err = client.Call("Str.Lower", args, &reply)
+		if err != nil {
+			log.Fatal("call error:", err)
+		}
+		time2 := time.Now()
+		elapsedTime := float64(time2.Sub(time1).Nanoseconds()) / 1000000
+		fmt.Fprintln(file, elapsedTime)
+
+		time.Sleep(10 * time.Millisecond)
+		// fmt.Printf("Lower: %s = %s\n", args.Txt, reply)
+
 	}
-	fmt.Printf("Lower: %s = %s\n", args.Txt, reply)
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
 }
