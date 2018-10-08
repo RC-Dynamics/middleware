cbdd92fd702c47344e8026828701dd660acab1d7
Roberto Fernandes <betocostaf@gmail.com>
2018-10-08 12:18:19 -0300

    Adding first version of ex5

--- ex5/client/clientRequestHandler.go
+++ ex5/client/clientRequestHandler.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
+	"runtime"
 )
 
 type Handler interface {
@@ -21,10 +24,10 @@ func (client *ClientRequestHandler) connect(address string) {
 	switch client.tp {
 	case "tcp":
 		client.handler = &ClientHandlerTCP{"", nil}
-	case "udp":
-		client.handler = &ClientHandlerUDP{"", nil}
-	case "rpc":
-		client.handler = &ClientHandlerRPC{"", nil, nil}
+		// case "udp":
+		// 	client.handler = &ClientHandlerUDP{"", nil}
+		// case "rpc":
+		// 	client.handler = &ClientHandlerRPC{"", nil, nil}
 	}
 	client.handler.connect(address)
 }
@@ -46,4 +49,145 @@ func checkError(err error) {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s ", err.Error())
 		os.Exit(1)
 	}
+}
+
+type ServerFunc interface {
+	toUpper(string) string
+	toLower(string) string
+}
+
+type ClientProxy struct {
+	address string
+}
+
+type StringProxy struct {
+	client_prxy ClientProxy
+}
+
+type Invocation struct {
+	address     string
+	method_name string
+	parameters  []string
+}
+
+type Termination struct {
+	result string
+}
+
+type MessageHeader struct {
+	magic       string
+	version     int
+	byteOrders  bool
+	messageType int
+	messageSize int
+}
+
+type RequestHeader struct {
+	context          string
+	id               int
+	responseExpected bool
+	objectKey        int
+	operation        string
+}
+
+type RequestBody struct {
+	parameters []string
+}
+
+type ReplyHeader struct {
+	serviceContext string
+	id             int
+	status         int
+}
+
+type ReplyBody struct {
+	result string
+}
+
+type MessageBody struct {
+	requestHeader RequestHeader
+	requestBody   RequestBody
+	replyHeader   ReplyHeader
+	replyBody     ReplyBody
+}
+
+type Message struct {
+	header MessageHeader
+	body   MessageBody
+}
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func (proxy *StringProxy) toUpper(str string) string {
+	inv := Invocation{
+		address:     proxy.client_prxy.address,
+		method_name: "toUpper",
+		parameters:  []string{str},
+	}
+	ter := invoke(inv)
+	return ter.result
+}
+
+func (proxy *StringProxy) toLower(str string) string {
+	inv := Invocation{
+		address:     proxy.client_prxy.address,
+		method_name: "toLower",
+		parameters:  []string{str},
+	}
+	ter := invoke(inv)
+	return ter.result
+}
+
+func invoke(inv Invocation) Termination {
+	chr := ClientRequestHandler{
+		tp: "tcp",
+	}
+	chr.connect(inv.address)
+	reqHeader := RequestHeader{
+		context:          "",
+		id:               0,
+		responseExpected: true,
+		objectKey:        0,
+		operation:        inv.method_name,
+	}
+
+	reqBody := RequestBody{
+		parameters: inv.parameters,
+	}
+
+	messageHeader := MessageHeader{
+		magic:       "MIOP",
+		version:     0,
+		byteOrders:  false,
+		messageType: 0,
+		messageSize: 0,
+	}
+
+	messageBody := MessageBody{
+		requestHeader: reqHeader,
+		requestBody:   reqBody,
+	}
+
+	msgtoBeMarshalled := Message{
+		header: messageHeader,
+		body:   messageBody,
+	}
+
+	msgMarshalled, _ := json.Marshal(msgtoBeMarshalled)
+
+	chr.send(msgMarshalled)
+
+	msgToBeUnmarshalled := chr.read(10)
+
+	var msgUnmarshalled Message
+
+	json.Unmarshal(msgToBeUnmarshalled, &msgUnmarshalled)
+
+	ter := Termination{
+		result: msgUnmarshalled.body.replybody.result,
+	}
+
+	return ter
 }
--- ex5/server/serverRequestHandler.go
+++ ex5/server/serverRequestHandler.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
+	"reflect"
+	"runtime"
+	"strings"
 )
 
 type Handler interface {
@@ -23,12 +27,12 @@ func (server *ServerHandler) create() {
 	case "tcp":
 		server.handler = &ServerHandlerTCP{server.port, nil}
 		// conn = handlerTCP.create(port)
-	case "udp":
-		server.handler = &ServerHandlerUDP{server.port, nil, nil}
-		// conn = handlerUDP.crete(port)
-	case "rpc":
-		server.handler = &ServerHandlerRPC{server.port}
-		// conn = handlerUDP.crete(port)
+		// case "udp":
+		// 	server.handler = &ServerHandlerUDP{server.port, nil, nil}
+		// 	// conn = handlerUDP.crete(port)
+		// case "rpc":
+		// 	server.handler = &ServerHandlerRPC{server.port}
+		// 	// conn = handlerUDP.crete(port)
 	}
 	server.handler.create()
 }
@@ -49,5 +53,141 @@ func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s ", err.Error())
 		os.Exit(1)
+	}
+}
+
+type ServerFunc interface {
+	toUpper(string) string
+	toLower(string) string
+}
+
+type ClientProxy struct {
+	address string
+}
+
+type StringProxy struct {
+	client_prxy ClientProxy
+}
+
+type Invocation struct {
+	address     string
+	method_name string
+	parameters  []string
+}
+
+type Termination struct {
+	result string
+}
+
+type MessageHeader struct {
+	magic       string
+	version     int
+	byteOrders  bool
+	messageType int
+	messageSize int
+}
+
+type RequestHeader struct {
+	context          string
+	id               int
+	responseExpected bool
+	objectKey        int
+	operation        string
+}
+
+type RequestBody struct {
+	parameters []string
+}
+
+type ReplyHeader struct {
+	serviceContext string
+	id             int
+	status         int
+}
+
+type ReplyBody struct {
+	result string
+}
+
+type MessageBody struct {
+	requestHeader RequestHeader
+	requestBody   RequestBody
+	replyHeader   ReplyHeader
+	replyBody     ReplyBody
+}
+
+type Message struct {
+	header MessageHeader
+	body   MessageBody
+}
+
+func GetFunctionName(i interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+}
+
+func (proxy *StringProxy) toUpper(str string) string {
+	return strings.ToUpper(str)
+}
+
+func (proxy *StringProxy) toLower(str string) string {
+	return strings.ToLower(str)
+}
+
+func invoke(clientProxy ClientProxy) Termination {
+	srh := ServerHandler{
+		tp:   "tcp",
+		port: clientProxy.address,
+	}
+	srh.create()
+
+	stringProxy := StringProxy{}
+	ter := Termination{}
+
+	for {
+		msgToBeUnmarshalled := srh.read(10)
+
+		var msgUnmarshalled Message
+
+		json.Unmarshal(msgToBeUnmarshalled, &msgUnmarshalled)
+
+		// TODO: processar
+		if msgUnmarshalled.body.requestHeader.operation == "toUpper" {
+			stringProxy.toUpper(msgUnmarshalled.body.requestBody.parameters[0])
+		} else if msgUnmarshalled.body.requestHeader.operation == "toLower" {
+			stringProxy.toLower(msgUnmarshalled.body.requestBody.parameters[0])
+		}
+
+		replyHeader := ReplyHeader{
+			serviceContext: "",
+			id:             0,
+			status:         0,
+		}
+
+		replyBody := ReplyBody{
+			result: ter.result,
+		}
+
+		messageHeader := MessageHeader{
+			magic:       "protocolo",
+			version:     0,
+			byteOrders:  false,
+			messageType: 0,
+			messageSize: 0,
+		}
+
+		messageBody := MessageBody{
+			replyHeader: replyHeader,
+			replyBody:   replyBody,
+		}
+
+		msgtoBeMarshalled := Message{
+			header: messageHeader,
+			body:   messageBody,
+		}
+
+		msgMarshalled, _ := json.Marshal(msgtoBeMarshalled)
+
+		srh.send(msgMarshalled)
+
 	}
 }
